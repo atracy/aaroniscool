089bc3b7225dc1c4afd1159059e2240b2d363c10
Aaron Tracy <atracy@gmail.com>
2013-01-16 13:20:55 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aaroniscool::Application.config.secret_token = '128be7b89d35c0886f8c8b9723495a1d5ca1e6dee56da77444055f23d46a14a2eea6044a266833ba1d8647aa13a5e7d4d40691bdecb862721a207e52b9a20b66'
